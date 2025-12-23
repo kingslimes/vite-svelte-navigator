166fe255f9f588c0b2debd12db03b4de09342a71
kingslime <72143717+kingslimes@users.noreply.github.com>
2025-12-23 16:49:07 +0700

    Update build.ts

--- build.ts
+++ build.ts
@@ -23,8 +23,8 @@ await $`mv ./dist/index.d.ts ./dist/main.d.ts`;
 
 // BUILD INDEX
 const files = [
-    { name: 'index.js', content: "export{default as Link}from'./Link.svelte'\nexport{default as RouterProvider}from'./Provider.svelte'\nexport{createBrowserRouter,useNavigate,useParams,link}from'./main'" },
-    { name: 'index.d.ts', content: "export{type BrowserRouter}from'./main'\nexport{default as Link}from'./Link.svelte'\nexport{default as RouterProvider}from'./Provider.svelte'\nexport{createBrowserRouter,useNavigate,useParams,link}from'./main'" }
+    { name: 'index.js', content:"export { default as Link } from './Link.svelte';\nexport { default as RouterProvider } from './Provider.svelte';\nexport { createBrowserRouter, useNavigate, useParams, link } from './main';" },
+    { name: 'index.d.ts', content:"export type { BrowserRouter } from './main';\nexport { default as Link } from './Link.svelte';\nexport { default as RouterProvider } from './Provider.svelte';\nexport { createBrowserRouter, useNavigate, useParams, link } from './main';" }
 ]
 for ( const file of files ) {
     await Bun.write(
@@ -38,3 +38,4 @@ await $`rsync -av --include='*/' --include='*.svelte' --exclude='*' src/ dist/`;
 await $`sed -i 's|'./navigator'|'./main'|g' dist/*.svelte`;
 await $`sed -i 's|'./namespace'|'./main'|g' dist/*.svelte`;
 await $`sed -i 's|'./utils'|'./main'|g' dist/*.svelte`;
+
